daf5fc04bc9868f1600a408b0dfc2ae3eb4c17b4
Richardson2021 <96339724+Richardson2021@users.noreply.github.com>
2022-05-09 17:52:16 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">react shopping cart</a>
+      </header>
+      <main>
+        Product List
+      </main>
+      <footer>all right</footer>
+    </div>
+  );
+}
+
+export default App;
